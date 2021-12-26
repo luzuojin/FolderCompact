f76f0ac42dd3790ef2ecfec95ac06c4c5b941e4e
luzj <luzuojin@gmail.com>
2021-12-26 11:20:25 +0800

    fix gradle sub module

--- src/main/java/dev/xframe/fc/FolderCompactPsiDirectoryNode.java
+++ src/main/java/dev/xframe/fc/FolderCompactPsiDirectoryNode.java
@@ -3,21 +3,22 @@ package dev.xframe.fc;
 import com.intellij.history.core.Paths;
 import com.intellij.ide.projectView.PresentationData;
 import com.intellij.ide.projectView.impl.nodes.PsiDirectoryNode;
-import com.intellij.openapi.roots.ProjectFileIndex;
-import com.intellij.openapi.vfs.VirtualFile;
 import org.jetbrains.annotations.NotNull;
 
 public class FolderCompactPsiDirectoryNode extends PsiDirectoryNode  {
 
-    public FolderCompactPsiDirectoryNode(PsiDirectoryNode origin) {
+    final PsiDirectoryNode root;
+
+    public FolderCompactPsiDirectoryNode(PsiDirectoryNode root, PsiDirectoryNode origin) {
         super(origin.getProject(), origin.getValue(), origin.getSettings(), origin.getFilter());
+        this.root = root;
     }
 
     @Override
     protected void updateImpl(@NotNull PresentationData data) {
         super.updateImpl(data);
         //set text like src/main/java...etc
-        data.setPresentableText(Paths.relativeIfUnder(getVirtualFile().getPath(), ProjectFileIndex.getInstance(getValue().getProject()).getContentRootForFile(getVirtualFile()).getPath()));
+        data.setPresentableText(Paths.relativeIfUnder(getVirtualFile().getPath(), root.getVirtualFile().getPath()));
     }
 
 }
--- src/main/java/dev/xframe/fc/FolderCompactTreeStructureProvider.java
+++ src/main/java/dev/xframe/fc/FolderCompactTreeStructureProvider.java
@@ -19,25 +19,25 @@ public class FolderCompactTreeStructureProvider implements TreeStructureProvider
     public @NotNull Collection<AbstractTreeNode<?>> modify(@NotNull AbstractTreeNode<?> parent, @NotNull Collection<AbstractTreeNode<?>> children, ViewSettings settings) {
         if(parent instanceof PsiDirectoryNode &&
                 ProjectRootsUtil.isModuleContentRoot(((PsiDirectoryNode) parent).getValue())) {
-            return children.stream().map(this::compact).flatMap(List::stream).collect(Collectors.toList());
+            return children.stream().map(child->compact((PsiDirectoryNode) parent, child)).flatMap(List::stream).collect(Collectors.toList());
         }
-        return  children;
+        return children;
     }
 
-    private List<AbstractTreeNode<?>> compact(AbstractTreeNode<?> node) {
-        List<AbstractTreeNode<?>> finded = findSourceRootChild(node, new ArrayList<>());
+    private List<AbstractTreeNode<?>> compact(PsiDirectoryNode root, AbstractTreeNode<?> node) {
+        List<AbstractTreeNode<?>> finded = findSourceRootChild(root, node, new ArrayList<>());
         return finded.isEmpty() ? Arrays.asList(node) : finded;
     }
 
-    private List<AbstractTreeNode<?>> findSourceRootChild(AbstractTreeNode<?> node, List<AbstractTreeNode<?>> out) {
+    private List<AbstractTreeNode<?>> findSourceRootChild(PsiDirectoryNode root, AbstractTreeNode<?> node, List<AbstractTreeNode<?>> out) {
         if((node instanceof PsiDirectoryNode)) {
             PsiDirectoryNode dir = (PsiDirectoryNode) node;
             if(ProjectRootsUtil.isModuleSourceRoot(dir.getVirtualFile(), dir.getProject())) {
-                out.add(new FolderCompactPsiDirectoryNode(dir));
+                out.add(new FolderCompactPsiDirectoryNode(root, dir));
                 return out;//不再继续往下找
             }
             for (AbstractTreeNode<?> child : node.getChildren()) {
-                findSourceRootChild(child, out);
+                findSourceRootChild(root, child, out);
             }
         }
         return out;
